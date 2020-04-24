b67d6a8ea643407eaadda9a44b33fbefa274f34b
Lapastoure Corentin <33086290+Lapastoure@users.noreply.github.com>
2020-04-24 13:24:47 +0200

    Edit risitas

--- Risitas.py
+++ Risitas.py
@@ -1,23 +1,19 @@
 import discord
 import CesiBot
+from CesiBot import bot
 from discord.ext import commands
 from discord.utils import get
 import os
 
-BOT_PREFIX = '/'
-
-bot = commands.Bot(command_prefix=BOT_PREFIX)
 
 @bot.command(pass_context=True, aliases=['i', 'issou'])
 async def issou(ctx):
 
     song_there = os.path.isfile("issou.mp3")
   
-    #await ctx.send("Getting everything ready now")
-
     voice = get(bot.voice_clients, guild=ctx.guild)
 
-    for file in os.listdir("./resources/song"):
+    for file in os.listdir("resources/song"):
         if file.endswith(".mp3"):
             name = file
 
@@ -26,5 +22,4 @@ async def issou(ctx):
     voice.source.volume = 0.5
 
     nname = name.rsplit("-", 2)
-    #await ctx.send(f"Playing: {nname[0]}")
     print("playing\n")
